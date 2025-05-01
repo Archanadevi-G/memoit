557e9521b1013733b36f521b8d8bd9c6b492e5ed
Archana G <archanadevig10@gmail.com>
2025-05-01 20:14:47 +0530

    deployment changes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -4,6 +4,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development" ? "http://localhost:8000" : "/api",
+    import.meta.env.MODE === "development" ? "http://localhost:8000" : "",
   withCredentials: true,
 });
