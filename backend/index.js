1c531469139870e0da301c3dcaa3d5c1ba61f73c
Archana G <archanadevig10@gmail.com>
2025-05-01 20:23:48 +0530

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -309,7 +309,7 @@ app.get("/search-notes/", authenticateToken, async (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
